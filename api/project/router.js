a19fc594bdc10297d5f46f51f9f914f5ffeed216
Paul Morrow <pmorow93@gmail.com>
2022-06-17 08:27:54 -0500

    setting up router

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,10 @@
 // build your `/api/projects` router here
+const router = require("express").Router();
+
+const Project = require("./model");
+
+router.get("/", async (req, res) => {
+  const project = await Project.find();
+});
+
+module.exports = router;

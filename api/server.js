3b749e3a9920a3e5bec166b6c4e324cecb56bc8c
Paul Morrow <pmorow93@gmail.com>
2022-06-17 08:05:50 -0500

    server built

--- api/server.js
+++ api/server.js
@@ -1,1 +1,9 @@
 // build your server here and require it from index.js
+const express = require("express");
+const server = express();
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({ message: err.message });
+});
+
+module.exports = server;
